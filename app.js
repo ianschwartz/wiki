f0eb9e3b67a6c19627cd19745c4bdbc813c91eeb
Schwartz <ian@schwartz.world>
2017-09-12 10:46:20 -0400

    Got the search function working, now when you search you get a UL with titles, content summaries and URLs

--- app.js
+++ app.js
@@ -1,54 +1,50 @@
-const ul = document.getElementById('data');
-var searchString = "";
-const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchString + '&limit=5&format=json&origin=*'; 
 
-function createNode(element) {
-	return document.createElement(element); // Create the type of element you pass in the parameters
+var vm = new Vue({
+	el: "#app",
+	data: {
+		searchString: "",
+		searchResults: [],
+		title: "test",
+		content: "Incididunt irure et ex dolor consectetur in sint anim qui sit mollit do laborum sit reprehenderit mollit mollit cillum veniam labore elit commodo irure sit dolore voluptate consectetur nulla dolor in qui aute adipisicing in dolore ut.",
+		wikiLink: "http://www.google.com"
+	},
+	methods: {
+		url: function(str) {
+			return 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + this.searchString + '&format=json&origin=*';
+		},
+		makeSearchReguest: function() {
+			if (this.searchStringIsThere) {
+				fetch(this.url())
+					.then(function(resp) {
+						return resp.json();
+					}).then(function(resp) {
+						vm.searchResults = zipThreeObjs(resp[1], resp[2], resp[3]);
+					}).catch(function(error) {
+						console.log(error);
+				});
+			} else {
+				console.log('no search terms');
+			}	
+		},
+		updateArticle: function(article) {
+			console.log(article);
+		}
+	},
+	computed: {
+		searchStringIsThere: function() {
+			return this.searchString.length > 0;
+		}
+	}
+})
+
+function zipThreeObjs(obj1, obj2, obj3) {
+	var arr = [];
+	Object.keys(obj1).forEach(function(key) {
+		arr.push({
+			title: obj1[key],
+			content: obj2[key],
+			url: obj3[key]
+		})
+	});
+	return arr;
 }
-
-function append(parent, el) {
-	return parent.appendChild(el); // Append the second parameter(element) to the first one
-}
-
-function wikiReq(url){
-	fetch(url)
-		.then((resp) => resp.json())
-		.then(function(data) {
-			var pages = data;
-			var searchTerm = pages[0];
-			var title = pages[1];
-			var content = pages[2];
-			console.log(pages[1]);
-			/*
-			ul.innerHTML += "\
-			<li class='listing'>\
-				<h2 class='title'>" + title + "</h2>\
-				<div class='content'>" +
-					content +
-				"</div>\
-			</li>"
-			console.log(pages);
-			*/
-
-		/*
-	  // Here you get the data to modify as you please
-	  	let authors = data.results;
-	  	return authors.map(function(author) {
-	  		let li = createNode('li');
-	  		let img = createNode('img');
-	  		let span = createNode('span');
-	  		img.src = author.picture.medium;
-	  		span.innerHTML = author.name.first + " " + author.name.last;
-	  		console.log(span.innerHTML);
-	  		append(li, img);
-	  		append(li, span);
-	  		append(ul, li);
-	  	})
-		*/
-	  }).catch(function(error) {
-	  // If there is any error you will catch them here
-	  console.log(error);
-	});   
-};
-
-wikiReq(url);
